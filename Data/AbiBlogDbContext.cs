5f948cbc10cec4c67a679d7e50c6c82f29283e0b
AlbionBurrniku <74713774+AlbionBurrniku2@users.noreply.github.com>
2023-05-23 22:48:40 +0200

    Add files via upload

---
+++ Data/AbiBlogDbContext.cs
@@ -0,0 +1,18 @@
+﻿using AbiBlog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AbiBlog.Data
+{
+    public class AbiBlogDbContext: DbContext
+    {
+       
+            public AbiBlogDbContext(DbContextOptions options) : base(options)
+            { }
+
+            public DbSet<Author> Authors { get; set; }
+            public DbSet<Post> Posts { get; set; }
+            public DbSet<Image> Images { get; set; }
+
+        }
+    }
+

12576ba843a20a5bd55646d17606a9460f0db215
AlbionBurrniku <74713774+AlbionBurrniku2@users.noreply.github.com>
2023-05-23 22:44:47 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+using AbiBlog.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AbiBlog
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<AbiBlogDbContext>(options =>
+      options.UseSqlServer(builder.Configuration
+      .GetConnectionString("AbiConnStr")));
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Posts}/{action=ViewPosts}/{id?}");
+
+            app.Run();
+        }
+    }
+}

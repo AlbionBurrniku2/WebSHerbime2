75e6d497d238ecc0af21f066efa24e80405bf97b
AlbionBurrniku <74713774+AlbionBurrniku2@users.noreply.github.com>
2023-05-23 22:48:03 +0200

    Add files via upload

---
+++ Models/Image.cs
@@ -0,0 +1,11 @@
+﻿namespace AbiBlog.Models
+{
+    public class Image
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public long Length { get; set; }
+        public string ContentType { get; set; }
+        public byte[] Data { get; set; }
+    }
+}
---
+++ Models/Post.cs
@@ -0,0 +1,11 @@
+﻿namespace AbiBlog.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Author { get; set; }
+    }
+}
